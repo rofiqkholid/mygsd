470cd65d3c21511dfc6037a22b53205a3d1da764
rofiqkholid <rofiqgege99@gmail.com>
2025-03-14 09:58:27 +0700

    Perbaikan dashboard

--- application/views/dashboard/dashboard.php
+++ application/views/dashboard/dashboard.php
@@ -1,4 +1,3 @@
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -6,8 +5,37 @@
 </head>
 
 <body class="bg-gray-100 flex">
-    <?php $this->load->view('layout/sidebar'); ?> 
-    <div id="content" class="content">
+    <?php $this->load->view('layout/sidebar'); ?>
+    <div id="content" class="content w-full">
         <h1 class="text-2xl font-bold">Dashboard Monitoring</h1>
+
+        <div class="admin-action grid grid-cols-6 gap-5 mt-5">
+            <a href="">
+                <div class="lost-found bg-blue-700 text-white p-2 rounded text-sm text-center">
+                    <span>Lost and Found</span>
+                </div>
+            </a>
+            <a href="">
+                <div class="e-tiketing bg-blue-700 text-white p-2 rounded text-sm text-center">
+                    <span>Request E-Tiketing</span>
+                </div>
+            </a>
+            <a href="">
+                <div class="e-permit bg-blue-700 text-white p-2 rounded text-sm text-center">
+                    <span>Request E-Permit</span>
+                </div>
+            </a>
+        </div>
+        <div class="data-monitoring gap-10 mt-10 grid grid-cols-3">
+            <div class="kehilangan bg-yellow-400 p-4 rounded font-bold text-center">
+                <h2 class="text-2xl mb-5">Kehilangan Barang</h2>
+                <span class="text-lg">0</span>
+            </div>
+            <div class="penemuan bg-green-400 p-4 rounded font-bold text-center">
+                <h2 class="text-2xl mb-5">Penemuan Barang</h2>
+                <span class="text-lg">0</span>
+            </div>
+        </div>
+
     </div>
 </body>
--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -1,5 +1,5 @@
 <script src="https://cdn.tailwindcss.com"></script>
-<link rel="stylesheet" href="assets/css/app.css">
+<link rel="stylesheet" href="../assets/css/app.css">
 
 
 <head>
@@ -12,7 +12,7 @@
 
         .sidebar.closed {
             transform: translateX(-70%);
-            width: 13rem;
+            width: 15rem;
         }
 
         .content {
@@ -55,27 +55,48 @@
         }
 
         .sidebar.closed .nav-item i {
-            margin-left: 7.7rem;
+            margin-left: 9.5rem;
             font-size: 1.5rem;
             font-style: normal;
         }
 
         .sidebar-logo {
+            display: flex;
+            align-items: center;
+            border-radius: 0.375rem;
+            transition: background-color 0.3s ease-in-out;
+        }
+
+        .sidebar-logo img {
+            transition: margin-left 0.3s ease-in-out;
+            margin-left: 0.5rem;
+            min-width: 2.6rem;
+            font-size: 1.5rem;
+            text-align: center;
+            font-style: normal;
+        }
+
+        .sidebar-logo span {
             transition: opacity 0.3s ease-in-out, margin-left 0.3s ease-in-out;
             opacity: 1;
-            text-align: center;
+            margin-left: 1.3rem;
             font-weight: bold;
-            font-size: 1.5rem;
-            margin-bottom: 1rem;
+            font-size: 1.1rem;
+            white-space: nowrap;
         }
 
-        .sidebar.closed .sidebar-logo {
+        .sidebar.closed .sidebar-logo span {
             opacity: 0;
+            margin-left: 100px;
+
         }
 
-        .sidebar.closed .icon-only {
-            display: block;
+        .sidebar.closed .sidebar-logo img {
+            margin-left: 8.9rem;
+
         }
+
+
 
         @media (max-width: 768px) {
             .sidebar {
@@ -95,27 +116,32 @@
 
 <body class="bg-gray-200">
     <div id="sidebar" class="sidebar fixed left-0 top-0 h-full bg-white shadow-md dark:bg-gray-900 p-4 closed z-50">
-        <div class="sidebar-logo">MyGSD Service</div>
-        <ul class="mt-10">
-            <li class="nav-item hover:bg-red-800 hover:text-white p-2 m-4">
-                <i class="bi bi-house-fill"></i>
-                <span>Dashboard</span>
-            </li>
-            <li class="nav-item hover:bg-red-800 hover:text-white p-2 m-4">
-                <i class="bi bi-person-fill"></i>
-                <span>Profile</span>
-            </li>
-            <li class="nav-item hover:bg-red-800 hover:text-white p-2 m-4">
-                <i class="bi bi-gear-fill"></i>
-                <span>Settings</span>
-            </li>
-            <a href="./">
+        <div class="sidebar-logo flex items-center p-2 m-4">
+            <img src="<?= base_url('public/assets/img/logo_college.png') ?>" alt="Logo" class="h-8 w-7">
+            <span class="ml-4">MyGSD Service</span>
+        </div>
+        <div class="menu-dashboard">
+            <ul class="mt-10">
                 <li class="nav-item hover:bg-red-800 hover:text-white p-2 m-4">
-                    <i class="bi bi-door-open-fill"></i>
-                    <span>Logout</span>
+                    <i class="bi bi-house-fill"></i>
+                    <span>Dashboard</span>
                 </li>
-            </a>
-        </ul>
+                <li class="nav-item hover:bg-red-800 hover:text-white p-2 m-4">
+                    <i class="bi bi-person-fill"></i>
+                    <span>Profile</span>
+                </li>
+                <li class="nav-item hover:bg-red-800 hover:text-white p-2 m-4">
+                    <i class="bi bi-gear-fill"></i>
+                    <span>Settings</span>
+                </li>
+                <a href="./">
+                    <li class="nav-item hover:bg-red-800 hover:text-white p-2 m-4">
+                        <i class="bi bi-door-open-fill"></i>
+                        <span>Logout</span>
+                    </li>
+                </a>
+            </ul>
+        </div>
     </div>
 
     <div id="content" class="content">
@@ -129,7 +155,7 @@
 
             function updateContentMargin() {
                 if (window.innerWidth > 768) {
-                    content.style.marginLeft = sidebar.classList.contains("closed") ? "0" : "12rem";
+                    content.style.marginLeft = sidebar.classList.contains("closed") ? "0" : "11rem";
                 } else {
                     content.style.marginLeft = "0";
                 }
